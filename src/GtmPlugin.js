f99a60a9b8272a3bbb76e1b73ea45228cfff0952
Manish Kumar <mib200@gmail.com>
2017-04-23 19:22:23 +0530

    run in browser only. SSR compatibility

--- src/GtmPlugin.js
+++ src/GtmPlugin.js
@@ -3,15 +3,19 @@ import pluginConfig from './config';
 /**
  * Plugin main class
  */
+var inBrowser = typeof window !== 'undefined';
+
 export default class AnalyticsPlugin {
 	trackView(screenName, path) {
-		logDebug('Dispatching TrackView', { screenName, path });
+		if (inBrowser) {
+			logDebug('Dispatching TrackView', { screenName, path });
 
-		let dataLayer = window.dataLayer = window.dataLayer || [];
-		dataLayer.push({
-			'event': 'content-view',
-			'content-name': path
-		});
+			let dataLayer = window.dataLayer = window.dataLayer || [];
+			dataLayer.push({
+				'event': 'content-view',
+				'content-name': path
+			});
+		}	
 	}
 
 	trackEvent({
@@ -22,17 +26,18 @@ export default class AnalyticsPlugin {
 		value = null,
 		noninteraction = false
 	} = {}) {
-		logDebug('Dispatching event', { category, action, label, value });
+		if (inBrowser) {
+			logDebug('Dispatching event', { category, action, label, value });
 
-		let dataLayer = window.dataLayer = window.dataLayer || [];
-		dataLayer.push({
-			'event': event || 'interaction',
-			'target': category,
-			'action': action,
-			'target-properties': label,
-			'value': value,
-			'interaction-type': noninteraction
-		});
+			let dataLayer = window.dataLayer = window.dataLayer || [];
+			dataLayer.push({
+				'event': event || 'interaction',
+				'target': category,
+				'action': action,
+				'target-properties': label,
+				'value': value,
+				'interaction-type': noninteraction
+			});
+		}	
 	}
-	
 }
