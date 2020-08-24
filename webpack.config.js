4d84fe78a263c379566466e4753282f8e79f5a80
Manish Kumar <mib200@gmail.com>
2020-08-25 02:27:55 +0530

    Updating package dependencies and webpack build config

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,27 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  mode: 'production',
   entry: [
     './src/index.js'
   ],
   output: {
-    filename: 'dist/vue-gtm.min.js',
+    filename: 'vue-gtm.min.js',
     libraryTarget: 'commonjs2'
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-  ],
   resolve: {
     extensions: [ '.js', '.json' ]
   },
+  optimization: {
+    minimize: true
+  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        query: {
-          presets: [ 'es2015', 'stage-2' ]
+        options: {
+          presets: ['@babel/preset-env']
         },
         exclude: /node_modules/
       }
